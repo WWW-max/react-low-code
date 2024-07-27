d51fa1dc5a0134f470d95f04f9bfada2bf3d0cd4
monster-wu <464711201@qq.com>
2024-07-27 12:01:55 +0800

    feat: 定义画布中组件Title和Input

---
+++ src/components/QuestionComponents/QuestionTitle/Component.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+import { Typography } from 'antd';
+import { QuestionTitleDefaultProps, QuestionTitlePropsType } from './interface';
+
+const { Title } = Typography;
+
+const QuestionTitle: FC<QuestionTitlePropsType> = (props: QuestionTitlePropsType) => {
+    const { text = '', level = 1, isCenter = false } = { ...QuestionTitleDefaultProps, ...props }
+    const getFontSizes = (level: number) => {
+        if (level === 1) return '24px';
+        if (level === 2) return '20px';
+        if (level === 3) return '16px';
+        return '16px';
+    }
+    return (
+        <Title
+          level={level}
+          style={{
+            textAlign: isCenter ? 'center' : 'start',
+            marginBottom: '0',
+            fontSize: getFontSizes(level),
+          }}
+        >
+            {text}
+        </Title>
+    )
+}
+
+export default QuestionTitle;
--- src/pages/question/Edit/EditCanvas.tsx
+++ src/pages/question/Edit/EditCanvas.tsx
@@ -1,8 +1,13 @@
 import React , { FC } from 'react';
+import QuestionTitle from '../../../components/QuestionComponents/QuestionTitle/Component';
+import QuestionInput from '../../../components/QuestionComponents/QuestionInput/Component';
 
 const EditCanvas: FC = () => {
     return (
-        <div>canvans</div>
+        <div>
+            <QuestionTitle text= "aaa" isCenter={true} level={2}/>
+            <QuestionInput />
+        </div>
     )
 }
 
