fdfebf175b119394ac4b5172a9836cdbea522aad
monster-wu <464711201@qq.com>
2024-07-27 11:02:00 +0800

    feat: 完成ui组件设计拆分,实现画布y轴滚动

---
+++ src/pages/question/Edit/EditCanvas.tsx
@@ -0,0 +1,9 @@
+import React , { FC } from 'react';
+
+const EditCanvas: FC = () => {
+    return (
+        <div>canvans</div>
+    )
+}
+
+export default EditCanvas
--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styles from './index.module.scss';
+import EditCanvas from './EditCanvas';
 
 export default function Edit() {
   const { id } = useParams();
-  return <div>Edit-{id}</div>;
+  return <div className={styles.container}>
+    <div >header</div>
+    <div className={styles['content-wrapper']}>
+      <div className={styles.content}>
+        <div className={styles.left}>left</div>
+        <div className={styles.main}>
+           <div className={styles['canvas-wrapper']}>
+              <EditCanvas />
+            </div>
+        </div>
+        <div className={styles.right}>right</div>
+      </div>
+    </div>
+  </div>;
 }
