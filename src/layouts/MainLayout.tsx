e830f396fa84363c1f4b18ac7260362861f3ac9d
WWW-max <464711201@qq.com>
2024-08-04 00:03:56 +0800

    refactor: 抽离Logo和UserInfo组件

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import styles from './MainLayout.module.scss';
 import { SnippetsFilled } from '@ant-design/icons';
-import { Layout } from 'antd';
+import { Layout, Spin } from 'antd';
 import useLoadUserData from '../hooks/useLoadUserData';
 import useNavPage from '../hooks/useNavPage';
+import Logo from '../components/Logo/Logo';
+import UserInfo from '../components/UserInfo/UserInfo';
 // mock.js只能拦截XMLHttpRequest请求，不能拦截fetch；
 // 直接导入使用上线前需要删除，不建议这样用
 // import '../_mock/index';
@@ -18,9 +20,7 @@ export default function MainLayout() {
   useNavPage(waitingUserData);
 
   const nav = useNavigate();
-  const goLogin = () => {
-    nav('/login');
-  };
+
   // useEffect(() => {
   //   axios.get('/api/test').then(res => console.log('axios data', res.data));
   // }, []);
@@ -28,17 +28,24 @@ export default function MainLayout() {
     <Layout>
       <Header className={styles.header}>
         <div className={styles.left} onClick={() => nav('/')}>
-          <SnippetsFilled />
-          <span style={{ marginLeft: '10px' }}>问卷低代码平台</span>
+          <Logo />
         </div>
         <div className={styles.right}>
-          <span onClick={goLogin}>登录</span>|<Link to="/register">注册</Link>
+          <UserInfo />
         </div>
       </Header>
-      <Content>
-        <Outlet />
-      </Content>
-      <Footer className={styles.footer}>问卷低代码平台&copy;2023 - preset. Created by wzp</Footer>
+      <Layout className={styles.main}>
+        <Content>
+          {waitingUserData ? (
+            <div style={{ textAlign: 'center', marginTop: '60px' }}>
+              <Spin />
+            </div>
+          ) : (
+            <Outlet />
+          )}
+        </Content>
+      </Layout>
+      <Footer className={styles.footer}>问卷低代码平台&copy;2024 - present. Created by wzp</Footer>
     </Layout>
   );
 }
