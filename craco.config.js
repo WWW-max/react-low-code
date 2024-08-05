d3521aaf64f0a499c3ef78edd3bec0ae50a7d8ed
WWW-max <464711201@qq.com>
2024-08-05 22:30:39 +0800

    perf: splitChunks分离代码抽离出公共代码使文件合理使用缓存

--- craco.config.js
+++ craco.config.js
@@ -6,4 +6,36 @@ module.exports = {
       '/api/*': 'http://localhost:8000',
     },
   },
+  /** 修改webpack配置 */
+  webpack: {
+    configure(webpackConfig) {
+      if (webpackConfig.mode === 'production') {
+        // 只在生产环境下抽离公共代码
+        if (webpackConfig.optimization == null) {
+          webpackConfig.optimization = {};
+        }
+        webpackConfig.optimization.splitChunks = {
+          chunks: 'all',
+          cacheGroups: {
+            antd: {
+              name: 'antd-chunk',
+              test: /antd/, // 正则匹配所有的antd文件
+              priority: 100, // 权重，值越大，优先抽离
+            },
+            reactDom: {
+              name: 'reactDom-chunk',
+              test: /react-dom/,
+              priority: 99,
+            },
+            vendors: {
+              name: 'vendors-chunk',
+              test: /node_modules/,
+              priority: 98,
+            },
+          },
+        };
+      }
+      return webpackConfig;
+    },
+  },
 };
